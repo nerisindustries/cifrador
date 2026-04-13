d6e57e0a8b6003f9ff2ba59b26ff46296277574b
nerisindustries <daviferreiraneris@gmail.com>
2026-04-13 20:37:56 -0300

    to na parte de ler

---
+++ main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+ public class Main{
+
+public static void main (String[]args){
+
+
+Scanner scanner = new Scanner (System.in);
+ 
+ System.out.println("Digite uma frase de até 47 carácteres para cifrarmos")
+ 
+ String frase = leitor. nextLine().toUpperCase();
+
+ char [] bank ={'ã','â','á','ä','ë','é','ê','ö','ô','õ','í','ü','ú','ç'};
+char [] banko ={'a','a','a','a','e','e','e','o','o','o','i','u','u','c'};
+
+char[] letrasFrase = frase.toCharArray();
+
+for (i= 0; i < letrasFrase.length(); i++){
+
+
+
+
+    
+
+
+
+
+}
+
+
+}
+
+
+
+
+ }

b3098ab5a07300d0b2420683bed20166a18d1df8
nerisindustries <daviferreiraneris@gmail.com>
2026-04-14 19:17:53 -0300

    arrumei com o davi teixeira o erro do código trocando o segundo ifi de lugar

--- main.java
+++ main.java
@@ -1,48 +1,39 @@
-import java.util.Scanner;
- public class Main{
+    import java.util.Scanner;
+    public class Main {
 
-public static void main (String[]args){
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+            System.out.println("Digite uma frase:");
+            String frase = scanner.nextLine().toUpperCase();
 
-Scanner scanner = new Scanner (System.in);
- 
- System.out.println("Digite uma frase de até 47 carácteres para cifrarmos");
- 
- String frase = scanner. nextLine().toUpperCase();
+            if (frase.length() > 47) {
+                System.out.println("Não é possivel cifrar, número maximo de caracteres excedido");
+            } else {
+                char[] bank = {'Ã', 'Â', 'Á', 'Ä', 'Ë', 'É', 'Ê', 'Ö', 'Ô', 'Õ', 'Í', 'Ü', 'Ú', 'Ç'};
+                char[] banko = {'A', 'A', 'A', 'A', 'E', 'E', 'E', 'O', 'O', 'O', 'I', 'U', 'U', 'C'};
 
-char[] bank = {'Ã','Â','Á','Ä','Ë','É','Ê','Ö','Ô','Õ','Í','Ü','Ú','Ç'};
-char[] banko = {'A','A','A','A','E','E','E','O','O','O','I','U','U','C'};
+                char[] letrasFrase = frase.toCharArray();
+                String novafrase = "";
 
-char[] letrasFrase = frase.toCharArray();
-String novafrase=" ";
-for (int i= 0; i < letrasFrase.length; i++){
-    for (int  b=0; b< bank.length; b++){
+                for (int i = 0; i < letrasFrase.length; i++) {
+                    char letraAtual = letrasFrase[i];
 
-if(letrasFrase[i]==bank[b]){
-    letrasFrase[i]= banko[b];
-    break;
-}
+                    for (int b = 0; b < bank.length; b++) {
+                        if (letraAtual == bank[b]) {
+                            letraAtual = banko[b];
+                            break;
+                        }
+                    }
 
+                    if (letraAtual >= 'A' && letraAtual <= 'Z') {
+                        novafrase = novafrase + letraAtual;
+                    }
+                }
 
+                System.out.println("Frase formatada: " + novafrase);
+            }
+
+            scanner.close();
+        }
     }
-
-if(letrasFrase[i]>='A' && letrasFrase[i]<='Z'){
-    novafrase= novafrase + letrasFrase[i];
-}
-
-
-    
-
-
-
-
-}
-System.out.println(novafrase);
-
-
-}
-
-
-
-
- }

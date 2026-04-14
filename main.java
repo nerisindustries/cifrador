272bf51f50ac223d6dd22aca1b15a96070dd68a5
nerisindustries <daviferreiraneris@gmail.com>
2026-04-13 21:22:18 -0300

    to na parte de ler e devolver tudo junto

--- main.java
+++ main.java
@@ -6,18 +6,29 @@ public static void main (String[]args){
 
 Scanner scanner = new Scanner (System.in);
  
- System.out.println("Digite uma frase de até 47 carácteres para cifrarmos")
+ System.out.println("Digite uma frase de até 47 carácteres para cifrarmos");
  
- String frase = leitor. nextLine().toUpperCase();
+ String frase = scanner. nextLine().toUpperCase();
 
- char [] bank ={'ã','â','á','ä','ë','é','ê','ö','ô','õ','í','ü','ú','ç'};
-char [] banko ={'a','a','a','a','e','e','e','o','o','o','i','u','u','c'};
+char[] bank = {'Ã','Â','Á','Ä','Ë','É','Ê','Ö','Ô','Õ','Í','Ü','Ú','Ç'};
+char[] banko = {'A','A','A','A','E','E','E','O','O','O','I','U','U','C'};
 
 char[] letrasFrase = frase.toCharArray();
+String novafrase=" ";
+for (int i= 0; i < letrasFrase.length; i++){
+    for (int  b=0; b< bank.length; b++){
 
-for (i= 0; i < letrasFrase.length(); i++){
+if(letrasFrase[i]==bank[b]){
+    letrasFrase[i]= banko[b];
+    break;
+}
 
 
+    }
+
+if(letrasFrase[i]>='A' && letrasFrase[i]<='Z'){
+    novafrase= novafrase + letrasFrase[i];
+}
 
 
     
@@ -26,6 +37,7 @@ for (i= 0; i < letrasFrase.length(); i++){
 
 
 }
+System.out.println(novafrase);
 
 
 }
